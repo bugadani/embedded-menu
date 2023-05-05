c196e60c58d9d7c402ebae1f11d9bfe0e66d4d4f
Dániel Buga <bugadani@gmail.com>
2023-05-05 18:38:02 +0200

    Add example that sets menu style

---
+++ examples/font.rs
@@ -0,0 +1,97 @@
+//! Run using `cargo run --example simple --target x86_64-pc-windows-msvc`
+//!
+//! Navigate using up/down arrows, interact using the Enter key
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_8X13_BOLD, iso_8859_1::FONT_6X10},
+    pixelcolor::BinaryColor,
+    prelude::{Point, Size},
+    primitives::Rectangle,
+    Drawable,
+};
+use embedded_graphics_simulator::{
+    sdl2::Keycode, BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent,
+    Window,
+};
+use embedded_menu::{
+    interaction::InteractionType,
+    items::{select::SelectValue, NavigationItem, Select},
+    Menu, MenuStyle,
+};
+
+#[derive(Copy, Clone)]
+pub enum TestEnum {
+    A,
+    B,
+    C,
+}
+
+impl SelectValue for TestEnum {
+    fn next(&self) -> Self {
+        match self {
+            TestEnum::A => TestEnum::B,
+            TestEnum::B => TestEnum::C,
+            TestEnum::C => TestEnum::A,
+        }
+    }
+
+    fn name(&self) -> &'static str {
+        match self {
+            TestEnum::A => "A",
+            TestEnum::B => "AB",
+            TestEnum::C => "ABC",
+        }
+    }
+}
+
+fn main() -> Result<(), core::convert::Infallible> {
+    let display_area = Rectangle::new(Point::zero(), Size::new(128, 64));
+    let mut menu = Menu::build_with_style(
+        "Menu",
+        display_area,
+        MenuStyle::new(BinaryColor::On, BinaryColor::On)
+            .with_font(&FONT_6X10)
+            .with_title_font(&FONT_8X13_BOLD),
+    )
+    .show_details_after(100)
+    .add_item(
+        NavigationItem::new("Nav item", ())
+            .with_marker("»") // not part of the ASCII font
+            .with_detail_text("Lorem ipsum dolor sit amet."),
+    )
+    .add_item(Select::new("Check this", true).with_detail_text("Description"))
+    .add_item(Select::new("Check this", false).with_detail_text("Description"))
+    .add_item(Select::new("Multiple options", TestEnum::A).with_detail_text("Description"))
+    .build();
+
+    let output_settings = OutputSettingsBuilder::new()
+        .theme(BinaryColorTheme::OledBlue)
+        .build();
+    let mut window = Window::new("Menu demonstration", &output_settings);
+
+    'running: loop {
+        let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(128, 64));
+        menu.update(&display);
+        menu.draw(&mut display).unwrap();
+        window.update(&display);
+
+        for event in window.events() {
+            match event {
+                SimulatorEvent::KeyDown {
+                    keycode,
+                    repeat: false,
+                    ..
+                } => match keycode {
+                    Keycode::Return => menu.interact(InteractionType::Select),
+                    Keycode::Up => menu.interact(InteractionType::Previous),
+                    Keycode::Down => menu.interact(InteractionType::Next),
+                    _ => None,
+                },
+                SimulatorEvent::Quit => break 'running,
+                _ => None,
+            };
+        }
+    }
+
+    Ok(())
+}
