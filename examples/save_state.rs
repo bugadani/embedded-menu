769f25b03f70f7d2c71783a98e5e75dea2a61c08
Dániel Buga <bugadani@gmail.com>
2023-08-07 10:27:59 +0200

    Remember menu data

--- examples/save_state.rs
+++ examples/save_state.rs
@@ -37,9 +37,23 @@ impl SelectValue for TestEnum {
     }
 }
 
+#[derive(Clone, Copy)]
+struct MenuData {
+    slice_data: [bool; 5],
+    select: TestEnum,
+}
+
+#[derive(Clone, Copy)]
+enum MenuEvent {
+    SliceCheckbox(usize, bool),
+    Select(TestEnum),
+    Nothing,
+}
+
 fn do_loop(
     window: &mut Window,
     state: &mut MenuState<Programmed, AnimatedPosition, Line>,
+    data: &mut MenuData,
     item_count: usize,
 ) -> bool {
     let style = MenuStyle::new(BinaryColor::On).with_animated_selection_indicator(10);
@@ -48,18 +62,33 @@ fn do_loop(
 
     for _ in 0..60 {
         let mut items = (0..item_count)
-            .map(|_| Select::new("Changing", false).with_detail_text("Description"))
+            .map(|i| {
+                Select::new("Changing", data.slice_data[i])
+                    .with_value_converter(match i {
+                        0 => |data| MenuEvent::SliceCheckbox(0, data),
+                        1 => |data| MenuEvent::SliceCheckbox(1, data),
+                        2 => |data| MenuEvent::SliceCheckbox(2, data),
+                        3 => |data| MenuEvent::SliceCheckbox(3, data),
+                        4 => |data| MenuEvent::SliceCheckbox(4, data),
+                        _ => panic!(),
+                    })
+                    .with_detail_text("Description")
+            })
             .take(item_count)
             .collect::<Vec<_>>();
 
         let mut menu = Menu::with_style(unsafe { std::mem::transmute(title.as_str()) }, style)
             .add_item(
-                NavigationItem::new("Foo", ())
+                NavigationItem::new("Foo", MenuEvent::Nothing)
                     .with_marker(">")
                     .with_detail_text("Some longer description text"),
             )
             .add_items(&mut items)
-            .add_item(Select::new("Check this too", TestEnum::A).with_detail_text("Description"))
+            .add_item(
+                Select::new("Check this too", data.select)
+                    .with_detail_text("Description")
+                    .with_value_converter(MenuEvent::Select),
+            )
             .build_with_state(*state);
 
         let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(128, 64));
@@ -69,7 +98,7 @@ fn do_loop(
         window.update(&display);
 
         for event in window.events() {
-            match event {
+            let change = match event {
                 SimulatorEvent::KeyDown {
                     keycode,
                     repeat: false,
@@ -83,6 +112,14 @@ fn do_loop(
                 SimulatorEvent::Quit => return false,
                 _ => None,
             };
+
+            if let Some(change) = change {
+                match change {
+                    MenuEvent::SliceCheckbox(idx, value) => data.slice_data[idx] = value,
+                    MenuEvent::Select(select) => data.select = select,
+                    MenuEvent::Nothing => {}
+                }
+            }
         }
 
         *state = menu.state();
@@ -99,19 +136,24 @@ fn main() -> Result<(), core::convert::Infallible> {
 
     let mut state = Default::default();
 
+    let mut data = MenuData {
+        slice_data: [false; 5],
+        select: TestEnum::A,
+    };
+
     'running: loop {
         for items in 1..6 {
-            if !do_loop(&mut window, &mut state, items) {
+            if !do_loop(&mut window, &mut state, &mut data, items) {
                 break 'running;
             }
         }
         for items in (2..5).rev() {
-            if !do_loop(&mut window, &mut state, items) {
+            if !do_loop(&mut window, &mut state, &mut data, items) {
                 break 'running;
             }
         }
         for items in 1..6 {
-            if !do_loop(&mut window, &mut state, items) {
+            if !do_loop(&mut window, &mut state, &mut data, items) {
                 break 'running;
             }
         }
