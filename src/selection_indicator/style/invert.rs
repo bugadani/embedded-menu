4619586af86d8d33ced5692135203ad457c53745
Dániel Buga <bugadani@gmail.com>
2023-08-22 07:43:42 +0200

    Bring Invert closer to Line, reduce padding

--- src/selection_indicator/style/invert.rs
+++ src/selection_indicator/style/invert.rs
@@ -18,19 +18,19 @@ impl IndicatorStyle for Invert {
     fn margin(&self, _state: &Self::State, _height: u32) -> Insets {
         Insets {
             left: 2,
-            top: 1,
-            right: 1,
-            bottom: 1,
+            top: 0,
+            right: 0,
+            bottom: 0,
         }
     }
 
-    fn shape(&self, _state: &Self::State, bounds: Rectangle, _fill_width: u32) -> Self::Shape {
-        bounds
+    fn shape(&self, _state: &Self::State, bounds: Rectangle, fill_width: u32) -> Self::Shape {
+        Rectangle::new(bounds.top_left, Size::new(fill_width, bounds.size.height))
     }
 
     fn draw<D>(
         &self,
-        _state: &Self::State,
+        state: &Self::State,
         input_state: InputState,
         display: &mut D,
     ) -> Result<u32, D::Error>
@@ -39,19 +39,16 @@ impl IndicatorStyle for Invert {
     {
         let display_area = display.bounding_box();
 
-        let fill_width = display_area.size.width + 1
+        let fill_width = display_area.size.width
             - if let InputState::InProgress(progress) = input_state {
                 interpolate(progress as u32, 0, 255, 0, display_area.size.width)
             } else {
                 0
             };
 
-        Rectangle::new(
-            display_area.top_left,
-            Size::new(fill_width, display_area.size.height),
-        )
-        .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
-        .draw(display)?;
+        self.shape(state, display_area, fill_width)
+            .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
+            .draw(display)?;
 
         Ok(fill_width)
     }
